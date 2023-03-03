c134d700d1c3d90e55e5031e3d21406cc4acead2
Joussemar Borges <joussemar.borges@gmail.com>
2023-03-03 17:54:16 -0300

    fix: [req4] refatorando testes utilizando o sinon.stub

---
+++ app/backend/src/tests/mocks/mockTeams.ts
@@ -0,0 +1,18 @@
+import Teams from "../../database/models/TeamsModel";
+
+const listTeams = [
+    {
+      id: 1,
+      teamName: "Avaí/Kindermann"
+    },
+    {
+      id: 2,
+      teamName: "Bahia"
+    },
+    {
+      id: 3,
+      teamName: "Botafogo"
+    },
+  ] as Teams[];
+
+export default listTeams;
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,43 +1,53 @@
-// import * as sinon from 'sinon';
+import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
 import {app} from '../app';
-// import Teams from '../database/models/TeamsModel';
+import listTeams from './mocks/mockTeams';
+import Teams from '../database/models/TeamsModel';
 
 // import { Response } from 'superagent';
+
 
 chai.use(chaiHttp);
 
 const {expect} = chai;
 
 describe('Testando o fluxo Teams', () => {
+
+
+  beforeEach(async () => {
+    sinon.stub(Teams, 'findAll').resolves(listTeams)
+    sinon.stub(Teams, 'findByPk').resolves(listTeams[0])
+  });
+
+  afterEach(() => {
+    (Teams.findAll as sinon.SinonStub).restore();
+    (Teams.findByPk as sinon.SinonStub).restore();
+  });
+
   it('Testando se o método GET retorna todos os times', async ()=> {
     
     //Arrange -> Mocks vão aqui,
-    const listTeams = [
-    {
-      id: 1,
-      teamName: "Avaí/Kindermann"
-    },
-    {
-      id: 2,
-      teamName: "Bahia"
-    },
-    {
-      id: 3,
-      teamName: "Botafogo"
-    },
-  ]
-    
+        
     //Action -> Ação ou chamada da função,
     const response = await chai.request(app).get('/teams')
-
 
     //assertion -> Se o resultado é o esperado "Expect",
     expect(response.status).to.be.equal(200);
     expect(response.body).to.deep.equal(listTeams);
+  })
 
+  it('Testando se o método GET retorna um id específico', async ()=> {
+    
+    //Arrange -> Mocks vão aqui,
+        
+    //Action -> Ação ou chamada da função,
+    const response = await chai.request(app).get('/teams/1')
+
+    //assertion -> Se o resultado é o esperado "Expect",
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(listTeams[0]);
   })
 })
