0b6acb5e7b63a540c6722c598cdb3b28adf6cd4b
Joussemar Borges <joussemar.borges@gmail.com>
2023-03-03 21:57:50 -0300

    feat: [req10] Valida o login de novo

--- app/backend/src/api/services/LoginService.ts
+++ app/backend/src/api/services/LoginService.ts
@@ -1,31 +1,32 @@
 import { ModelStatic } from 'sequelize';
+import * as bcrypt from 'bcryptjs';
 import User from '../../database/models/UsersModel';
 import JWT from '../Utils/token/JWT';
 import loginShcema from '../Utils/Validations/LoginSchema';
-// import mapError from '../errors/mapError';
+import MapError from '../errors/mapError';
 
 class LoginService {
   model: ModelStatic<User> = User;
   jwt = new JWT();
 
   async validateLogin(email: string, password: string): Promise<object> {
-    const result = await this.model.findOne({ where: { email } });
-
     const { error } = loginShcema.validate({ email, password });
 
-    if (error) {
-      return { message: error.message };
-    }
+    const result = await this.model.findOne({ where: { email } });
+
+    if (error || !result) throw new MapError('Invalid email or password', '401');
+
+    const comparePassword = bcrypt.compareSync(password, result.password);
+    if (!comparePassword) throw new MapError('Invalid email or password', '401');
 
     const payload = {
-      id: result?.id,
-      email: result?.email,
-      username: result?.username,
-      role: result?.role,
+      id: result.id,
+      email: result.email,
+      username: result.username,
+      role: result.role,
     };
 
     const token = { token: this.jwt.tokenGenerate(payload) };
-
     return token;
   }
 }
