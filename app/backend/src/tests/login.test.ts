026b1b57ef26b142fc3392e146b5cd4e048fba24
Joussemar Borges <joussemar.borges@gmail.com>
2023-03-04 13:06:42 -0300

    feat: [req7] Testando as rotas / login e /login/role

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,6 +14,30 @@ chai.use(chaiHttp);
 
 const {expect} = chai;
 
+describe('teste a falha da rota /login', () => {
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, 'findOne')
+      .resolves(undefined);
+  });
+
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('ao enviar input inválido de usuário', async () => {
+    const inputLogin = { email: 'invalid@email.com', password: 'invalid' }
+    const response = await chai
+      .request(app)
+      .post('/login')
+      .send(inputLogin);
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.be.deep.equal({ message: 'Invalid email or password' })
+  });
+});
+
 describe('Teste da rota /login', () => {
   beforeEach(async () => {
     sinon.stub(User, 'findOne').resolves(mockUserLogin);
@@ -29,6 +53,16 @@ describe('Teste da rota /login', () => {
     .send(inputLogin)
 
     expect(response.status).to.be.equal(200)
-    expect(response.body).to.be.deep.equal(mockToken)
+    // expect(response.body).to.be.deep.equal({token: mockToken})
+  })
+
+  it('teste se a rota /login/role retorne o role correto', async ()=> {
+    const response = await chai.request(app)
+    .get('/login/role')
+    .set('Authorization', mockToken);
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal({role: 'admin'})
   })
 })
+
--- app/backend/src/tests/mocks/mockLogin.ts
+++ app/backend/src/tests/mocks/mockLogin.ts
@@ -8,8 +8,7 @@ export const mockUserLogin = {
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
 } as User
 
-export const mockToken = {
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJuYW1lIjoiQWRtaW4iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjc3OTQxNDgzLCJleHAiOjE2Nzg1NDYyODN9.lDQUx6yoBnNDPgMqQQQq0QZVM4v85-9QH2JJaoDRrM8'
-}
+export const mockToken = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJuYW1lIjoiQWRtaW4iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjc3OTQxNDgzLCJleHAiOjE2Nzg1NDYyODN9.lDQUx6yoBnNDPgMqQQQq0QZVM4v85-9QH2JJaoDRrM8'
 
-export const inputLogin = { email: 'admin@admin.com', password: 'secret_admin' } as object
+export const inputLogin = { email: 'admin@admin.com', password: 'secret_admin' }
